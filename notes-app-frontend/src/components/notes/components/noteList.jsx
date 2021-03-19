11c7097b69d7b14f2c20043e7903778fd7472882
Rohith Ramanathan <rohram04@gmail.com>
2021-03-19 13:25:59 -0700

    :sparkles: Lazyload Notes
    lazy loading notes based off of screen size

--- notes-app-frontend/src/components/notes/components/noteList.jsx
+++ notes-app-frontend/src/components/notes/components/noteList.jsx
@@ -1,49 +1,80 @@
 import { Button } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Header } from "../../header/header";
 import Note from "./note";
 import "../notes.css";
+import { Waypoint } from "react-waypoint";
 
 const axios = require("axios");
 
 export default function NoteList(props) {
-  const [offset, setOffset] = useState(0);
-  const [notesList, setNotesList] = useState([]);
+  const [notesList, setNotesList] = useState({
+    offsetIncrement: 20,
+    offset: 0,
+    notesList: [],
+  });
+  const grid = useRef(null);
 
   useEffect(() => {
-    getNotes();
-  }, []);
+    getCellCount(grid.current);
 
-  const getNotes = (refresh = false) => {
-    const currentOffset = refresh ? 0 : offset;
-    const currentNotesList = refresh ? [] : notesList;
+    const handleResize = () => {
+      const cellCount = getCellCount(grid.current);
+      setNotesList((prevState) => ({ ...prevState, increment: cellCount }));
+    };
+
+    window.addEventListener("resize", debounce(handleResize, 250));
+    return () => {
+      window.removeEventListener("resize", debounce(handleResize, 250));
+    };
+  }, [grid]);
+
+  //REVIEW: Function needs to be refactored - very hard to read
+  const getNotes = async (refresh = false) => {
+    const { offsetIncrement, offset } = notesList;
+    if (offset === -1 && !refresh) return;
+
+    const newOffset = refresh ? 0 : offset;
+    const currentNotesList = refresh ? [] : notesList.notesList;
     axios
       .get("/api/notes", {
-        params: { offset: currentOffset },
+        params: { offset, limit: offsetIncrement },
       })
       .then(({ data }) => {
-        console.log("data", data);
-        setNotesList([...currentNotesList, ...data.notes]);
-        setOffset(currentOffset + data.notes.length);
+        setNotesList((prevState) => ({
+          notesList: [...currentNotesList, ...data.notes],
+          current:
+            data.notes.length < prevState.increment
+              ? -1
+              : newOffset + data.notes.length,
+        }));
       });
+  };
+
+  const lazyLoadNotes = ({ previousPosition, currentPosition, event }) => {
+    if (previousPosition === "inside") return;
+    getNotes();
   };
 
   return (
     <React.Fragment>
       <Header demo={props.demo} user={props.user} />
       <div className="notesList">
-        <div className="container">
-          {notesList.map((note) => (
+        <div ref={grid} className="container">
+          {notesList.notesList.map((note) => (
             <Note
               id={note.noteid}
               getNotes={getNotes}
-              classNames="note"
+              className="note"
               note={note}
               onClick={() => props.openNote(note.noteid)}
               accessToken={props.accessToken}
             />
           ))}
+          <Waypoint onEnter={lazyLoadNotes}>
+            <div className="waypoint"></div>
+          </Waypoint>
         </div>
         <Button
           className="button"
@@ -58,3 +89,33 @@ export default function NoteList(props) {
     </React.Fragment>
   );
 }
+
+const debounce = (func, time) => {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(func, time);
+  };
+};
+
+const getCellCount = (grid) => {
+  const gridComputedStyle = window.getComputedStyle(grid);
+
+  const gridHeight = parseInt(
+    gridComputedStyle.getPropertyValue("height").replace("px", "")
+  );
+  // get number of grid rows
+
+  const gridRowHeight = parseInt(
+    gridComputedStyle.getPropertyValue("grid-auto-rows").replace("px", "")
+  );
+
+  const gridRowCount = Math.floor(gridHeight / gridRowHeight);
+
+  // get number of grid columns
+  const gridColumnCount = gridComputedStyle
+    .getPropertyValue("grid-template-columns")
+    .split(" ").length;
+
+  return gridColumnCount * gridRowCount;
+};
