3fc63b8cd50e83c5bd5c7126d5cad81c86920efe
Rohith Ramanathan <rohram04@gmail.com>
2021-03-21 02:46:43 -0400

    :sparkles: Popup login while in Pwa mode

--- notes-app-frontend/src/components/Home/Home.js
+++ notes-app-frontend/src/components/Home/Home.js
@@ -3,6 +3,13 @@ import React from "react";
 import "./Home.css";
 import { DemoButton, LoginButton, SignupButton } from "./HomeButtons";
 
+function isPwa() {
+  return ["fullscreen", "standalone", "minimal-ui"].some(
+    (displayMode) =>
+      window.matchMedia("(display-mode: " + displayMode + ")").matches
+  );
+}
+
 const Home = () => {
   return (
     <div className="home">
--- notes-app-frontend/src/components/Home/HomeButtons.js
+++ notes-app-frontend/src/components/Home/HomeButtons.js
@@ -1,17 +1,38 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./Home.css";
 
+function isPwa() {
+  return ["fullscreen", "standalone", "minimal-ui"].some(
+    (displayMode) =>
+      window.matchMedia("(display-mode: " + displayMode + ")").matches
+  );
+}
+
+const useLoginAuth0 = () => {
+  const { loginWithPopup, loginWithRedirect } = useAuth0();
+  const history = useHistory();
+
+  const login = isPwa()
+    ? async (args = {}) => {
+        loginWithPopup(args);
+        history.push("/notes");
+      }
+    : (args = {}) => loginWithRedirect(args);
+
+  return login;
+};
+
 export const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const login = useLoginAuth0();
   return (
     <Button
       className="homebutton"
       size="large"
       variant="outlined"
-      onClick={() => loginWithRedirect()}
+      onClick={login}
     >
       login
     </Button>
@@ -19,17 +40,13 @@ export const LoginButton = () => {
 };
 
 export const SignupButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const login = useLoginAuth0();
   return (
     <Button
       className="homebutton"
       size="large"
       variant="outlined"
-      onClick={() =>
-        loginWithRedirect({
-          screen_hint: "signup",
-        })
-      }
+      onClick={() => login({ screen_hint: "signup" })}
     >
       signup
     </Button>
@@ -49,3 +66,25 @@ export const DemoButton = () => {
     </Button>
   );
 };
+
+//Function used to immediately redirect user to auth0 login if they are using a pwa
+// const useRedirectToLoginPwa = () => {
+//   const [pwa, setPwa] = useState(null);
+
+//   const { loginWithPopup } = useAuth0();
+//   const history = useHistory();
+
+//   useLayoutEffect(() => {
+//     const login = async () => {
+//       if (pwa !== null) return;
+//       await loginWithPopup();
+//       history.push("/notes");
+//       setPwa(true);
+//     };
+
+//     if (isPwa() && pwa === null) login();
+//     if (!isPwa() && pwa === null) setPwa(false);
+//   });
+
+//   return pwa;
+// };
